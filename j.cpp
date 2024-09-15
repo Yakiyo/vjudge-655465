9a1bd81ba220e8cba9357dceb908463767366376
Yakiyo <yakiyo1206@gmail.com>
2024-09-15 17:33:13 +0600

    feat: j
    - sort the vector
    - use binary search so that it doesnt exceed allowed time

--- j.cpp
+++ j.cpp
@@ -13,8 +13,10 @@ int main(){
         nums.push_back(j);
     }
 
+    std::sort(nums.begin(), nums.end());
+
     for (int i = 1; i <= n; i++) {
-        if (!(std::find(nums.begin(), nums.end(), i) != nums.end())) {
+        if (!(std::binary_search(nums.begin(), nums.end(), i))) {
             std::cout << i;
             return 0;
         }
