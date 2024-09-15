bafeaa5ded04da6cde9e15b8bda4e449ee65fc08
Yakiyo <yakiyo1206@gmail.com>
2024-09-15 18:44:01 +0600

    feat: l
    - no idea why it fails on test 1

--- l.cpp
+++ l.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <vector>
 #include <cstdint>
+#include <cmath>
 using namespace std;
 
 bool findOddDiv(int64_t n) {
     // if num itself is odd, then it itself is an odd divisor
     if (!((n % 2) == 0)) return true;
-    for (int64_t i = 3; i < n; i += 2) {
-        if ((n % i) == 0) return true;
+    
+    vector<int64_t> divs{};
+    int64_t root{ (int64_t) sqrt(n) + 1 };
+    for (int64_t i = 2; i <= root; i++) {
+        if (!(n % i == 0)) continue;
+        int64_t j = n/i;
+        if ((i == 1) || (j == 1)) continue;
+        if (!(i % 2 == 0) || !(j % 2 == 0)) {
+            return true;
+        }
     }
+
     return false;
 }
 
@@ -22,7 +33,6 @@ int main(){
         } else {
             cout << "NO";
         }
-        cout << "\n\n";
     }
     return 0;
 }
